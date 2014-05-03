45eec27b9a0c1bfd5bf02a099954f71dc5885802
Thomas Champagne <th.champagne@gmail.com>
2014-05-03 15:36:21 +0200

    Fast behind cam Plate keyboard support

--- Assets/sources/main/camera/impl/BehindCameraControllerImpl.cs
+++ Assets/sources/main/camera/impl/BehindCameraControllerImpl.cs
@@ -11,7 +11,7 @@ public class BehindCameraControllerImpl : AbstractCameraController, CameraContro
 	{
 		Vector3 invertFwdOffset = tracker.transform.forward * -10;
 		invertFwdOffset.y += 4;
-		camera.transform.position = Vector3.Lerp (camera.transform.position, tracker.transform.position + invertFwdOffset, Time.deltaTime * 4f);
+		camera.transform.position = Vector3.Lerp (camera.transform.position, tracker.transform.position + invertFwdOffset, Time.deltaTime * 8f);
 		camera.transform.LookAt (tracker.transform.position);
 	}
 	
