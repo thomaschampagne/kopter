34417d8667ebaa20ae40e332e33d378c677a640e
Thomas Champagne <th.champagne@gmail.com>
2014-05-03 16:42:44 +0200

    Clean

--- Assets/sources/main/kopter/GameController.cs
+++ Assets/sources/main/kopter/GameController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	protected MotorControlsMessage mMotorControlsMessage = null, mPreviousControlsMessage = null;
+	protected MotorControlsMessage mMotorControlsMessage = null;
 
 	// Use this for initialization
 	void Start () {
