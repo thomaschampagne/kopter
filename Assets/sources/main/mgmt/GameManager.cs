6e7b74a23b12243fcbffcf48fc07457aba38050b
Thomas Champagne <th.champagne@gmail.com>
2014-05-04 02:34:11 +0200

    Comments user tracking

--- Assets/sources/main/mgmt/GameManager.cs
+++ Assets/sources/main/mgmt/GameManager.cs
@@ -50,8 +50,9 @@ public class GameManager : MonoBehaviour
 	void handleSplash ()
 	{
 		// Track a new game session
-		if (Application.platform != RuntimePlatform.WindowsEditor)
-			StartCoroutine (registerGamesession ());
+		if (Application.platform != RuntimePlatform.WindowsEditor) {
+//			StartCoroutine (registerGamesession ());
+		}
 		
 		// Load main level
 		startLoadingLevel (Level.main); // GameManager Ready setting first state and load scene	
@@ -97,71 +98,72 @@ public class GameManager : MonoBehaviour
 		}
 	}
     
-	IEnumerator OnApplicationQuit ()
+	void OnApplicationQuit ()
 	{
-		if (Application.platform != RuntimePlatform.WindowsEditor)
-			yield return StartCoroutine(endGamesession ());
+//		if (Application.platform != RuntimePlatform.WindowsEditor)
+//			yield return StartCoroutine(endGamesession ());
 	}
 	
 	// Use this for initialization
-	IEnumerator registerGamesession ()
-	{
-        
-		Dictionary<String, String> parameters = new Dictionary<String, String> ();
-		/**
-        * GET Params
-        */
-		parameters.Add ("method", "newSession");
-		parameters.Add ("version", K.version.ToString());
-		parameters.Add ("unityVersion", Application.unityVersion);
-		parameters.Add ("hostname", SystemInfo.deviceName);
-		parameters.Add ("os", SystemInfo.operatingSystem);
-		parameters.Add ("platform", Application.platform.ToString());
-		parameters.Add ("resolution", Screen.width + ":" + Screen.height);
-		parameters.Add ("gc", SystemInfo.graphicsDeviceName);
-		parameters.Add ("cpu", SystemInfo.processorType);
-		parameters.Add ("devicemodel", SystemInfo.deviceModel);
 
-		HttpRest httpRest = new HttpRest ();
-		httpRest.Method = HttpRest.SendMethod.POST;
-		httpRest.Url = K.wsUrl;
-		httpRest.Parameters = parameters;
-                
-		yield return StartCoroutine(httpRest.call());   // Wait for webservice reponse
-                	
-		if (httpRest.isSuccess ()) {
-			
-			JSONObject jsonParser = new JSONObject (httpRest.HttpResponse.text);
-			JSONObject r = jsonParser.GetField ("r");
-			gameSessionId = r.ToString ().Replace ("\"", "");
-			Debug.Log ("gameSession is : " + gameSessionId);
-		} else {
-			Debug.LogError ("No gameSession, error: " + httpRest.HttpResponse.error);
-		}
-	}
-	
-	IEnumerator endGamesession ()
-	{
-		
-		Dictionary<String, String> parameters = new Dictionary<String, String> ();
-		parameters.Add ("method", "endSession");
-		parameters.Add ("sessionId", gameSessionId);
-		
-		HttpRest httpRest = new HttpRest ();
-		httpRest.Method = HttpRest.SendMethod.POST;
-		httpRest.Url = K.wsUrl;
-		httpRest.Parameters = parameters;
-                
-		yield return StartCoroutine(httpRest.call());   // Wait for webservice reponse
-                	
-		if (httpRest.isSuccess ()) {
-			Debug.Log ("END session response is : " + httpRest.HttpResponse.text);
-			
-		} else {
-			Debug.LogError ("END session error is : " + httpRest.HttpResponse.error);
-		}
-        
-	}
+//	IEnumerator registerGamesession ()
+//	{
+//        
+//		Dictionary<String, String> parameters = new Dictionary<String, String> ();
+//		/**
+//        * GET Params
+//        */
+//		parameters.Add ("method", "newSession");
+//		parameters.Add ("version", K.version.ToString());
+//		parameters.Add ("unityVersion", Application.unityVersion);
+//		parameters.Add ("hostname", SystemInfo.deviceName);
+//		parameters.Add ("os", SystemInfo.operatingSystem);
+//		parameters.Add ("platform", Application.platform.ToString());
+//		parameters.Add ("resolution", Screen.width + ":" + Screen.height);
+//		parameters.Add ("gc", SystemInfo.graphicsDeviceName);
+//		parameters.Add ("cpu", SystemInfo.processorType);
+//		parameters.Add ("devicemodel", SystemInfo.deviceModel);
+//
+//		HttpRest httpRest = new HttpRest ();
+//		httpRest.Method = HttpRest.SendMethod.POST;
+//		httpRest.Url = K.wsUrl;
+//		httpRest.Parameters = parameters;
+//                
+//		yield return StartCoroutine(httpRest.call());   // Wait for webservice reponse
+//                	
+//		if (httpRest.isSuccess ()) {
+//			
+//			JSONObject jsonParser = new JSONObject (httpRest.HttpResponse.text);
+//			JSONObject r = jsonParser.GetField ("r");
+//			gameSessionId = r.ToString ().Replace ("\"", "");
+//			Debug.Log ("gameSession is : " + gameSessionId);
+//		} else {
+//			Debug.LogError ("No gameSession, error: " + httpRest.HttpResponse.error);
+//		}
+//	}
+//	
+//	IEnumerator endGamesession ()
+//	{
+//		
+//		Dictionary<String, String> parameters = new Dictionary<String, String> ();
+//		parameters.Add ("method", "endSession");
+//		parameters.Add ("sessionId", gameSessionId);
+//		
+//		HttpRest httpRest = new HttpRest ();
+//		httpRest.Method = HttpRest.SendMethod.POST;
+//		httpRest.Url = K.wsUrl;
+//		httpRest.Parameters = parameters;
+//                
+//		yield return StartCoroutine(httpRest.call());   // Wait for webservice reponse
+//                	
+//		if (httpRest.isSuccess ()) {
+//			Debug.Log ("END session response is : " + httpRest.HttpResponse.text);
+//			
+//		} else {
+//			Debug.LogError ("END session error is : " + httpRest.HttpResponse.error);
+//		}
+//        
+//	}
 }
 
 
