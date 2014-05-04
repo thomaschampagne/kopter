b99569943eaec31706411b8f93f56870be437bc8
Thomas Champagne <th.champagne@gmail.com>
2014-05-04 12:49:31 +0200

    first reorder

--- Assets/sources/main/camera/CameraControllerInterface.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface CameraControllerInterface {
-	void InitCam ();
-	void LateUpdateCam();
-	void setCamera(Camera camera);
-	void setTracker(Transform tracker);
-}
