acb3ea2ec4ddf63c7c447fc8456e7ae264c31a9c
Thomas Champagne <th.champagne@gmail.com>
2016-02-24 15:29:48 +0100

    Create github page

--- Assets/Scripts/camera/AbstractCameraController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public abstract class AbstractCameraController : ScriptableObject {
-	
-	protected Camera camera;
-	protected Transform tracker;
-	
-}
